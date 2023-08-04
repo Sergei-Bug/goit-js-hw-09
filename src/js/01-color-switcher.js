51f3d2541f20d578fc85ab8602de88460b71a540
Sergei-Bug <122543402+Sergei-Bug@users.noreply.github.com>
2023-08-04 16:30:32 +0300

    Initialization

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,42 @@
+const refs = {
+  body: document.body,
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+
+refs.start.addEventListener('click', onButtonStart);
+refs.stop.addEventListener('click', onButtonStop);
+
+let id = null;
+
+function onButtonStart(event) {
+  if (refs.body.dataset.active === 'true') {
+    return;
+  }
+
+  refs.body.dataset.active = true;
+  changeColor();
+
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+
+  id = setInterval(changeColor, 1000);
+}
+
+function onButtonStop(event) {
+  clearInterval(id);
+
+  refs.body.dataset.active = false;
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
+
+function changeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
